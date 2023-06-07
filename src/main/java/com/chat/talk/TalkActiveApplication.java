2f8ddf217186762980915171a61f3a2370f7ab9d
Jitender <jitendrayadav@mygetepay.com>
2023-06-07 18:30:14 +0530

    first commit

---
+++ src/main/java/com/chat/talk/TalkActiveApplication.java
@@ -0,0 +1,13 @@
+package com.chat.talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkActiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkActiveApplication.class, args);
+	}
+
+}
